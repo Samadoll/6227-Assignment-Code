f12da1a7803c98267b6399b0c0a66cffda26f7d5
Jiawei Tan <jiaweitan@Jwww.local>
2023-03-18 18:12:33 +0800

    A3, gitignore

---
+++ Assignment-3/kmean.py
@@ -0,0 +1,133 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import time
+import os
+
+
+def get_random_point(n, min_distance):
+    np.random.seed(1333)
+    points = []
+    while len(points) < n:
+        point = np.random.rand(2)
+        if all(np.linalg.norm(point - existing_point) >= min_distance for existing_point in points):
+            points.append(point)
+    return points
+
+
+def get_initial_points():
+    initial_points = [
+        [0, 0],
+        [30, 0],
+        [0, 40],
+        [100, 0],
+        [50, 50]
+    ]
+    return initial_points
+
+
+def get_random_within_r(point, radius):
+    points = []
+    for i in range(20):
+        angle = random.uniform(0, 2*math.pi)
+        distance = random.uniform(0, radius)
+        points.append([point[0] + distance * math.cos(angle), point[1] + distance * math.sin(angle)])
+    return points
+
+
+def plot_points(points, labels, centers):
+    plt.scatter([x[0] for x in points], [x[1] for x in points], c=labels)
+    plt.scatter([x[0] for x in centers], [x[1] for x in centers], marker='*', s=200, linewidth=3, color='r')
+    # plt.axis('off')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+def kmean(points):
+    np.random.seed(0)
+    X = np.random.randn(100, 2)
+    print(type(X))
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(points)
+
+    # Get the cluster centers and labels
+    centers = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
+    plot_points(points, labels, centers)
+    # Plot the data points and cluster centers
+    # plt.scatter(X[:, 0], X[:, 1], c=labels)
+    # plt.scatter(centers[:, 0], centers[:, 1], marker='*', s=200, linewidth=3, color='r')
+    # plt.show()
+
+
+def kmean_n_step(points):
+    data = np.array(points)
+    centroids = np.array([
+        [0, 10],
+        [20, 10],
+        [0, 30],
+        [40, 10],
+        [10, 30]
+    ])
+    centroids_b = np.array([
+        [95, -5],
+        [95, 5],
+        [105, -5],
+        [105, 5],
+        [100, 0]
+    ])
+    t = round(time.time())
+    os.makedirs(str(t))
+    plot_kmean_n_step(str(t), data, centroids, "A")
+    plot_kmean_n_step(str(t), data, centroids_b, "B")
+
+
+def plot_kmean_n_step(folder, data, centroids, success):
+    plt.scatter(data[:,0], data[:,1], color='black')
+    plt.scatter(centroids[:,0], centroids[:,1], color='red')
+    
+
+    # Iterate through the K-means algorithm
+    for i in range(5):
+        # Assign each data point to the closest centroid
+        distances = np.sqrt(((data - centroids[:, np.newaxis])**2).sum(axis=2))
+        labels = np.argmin(distances, axis=0)
+        
+        # Plot the data points and centroids
+        plt.figure()
+        plt.scatter(data[:,0], data[:,1], c=labels, cmap='viridis')
+        plt.scatter(centroids[:,0], centroids[:,1], color='red')
+        plt.savefig(f"{folder}/{success}_{i}_1.png")
+        
+        # Update the centroids based on the new cluster assignments
+        centroids = np.array([data[labels == j].mean(axis=0) for j in range(len(centroids))])
+        
+        # Plot the updated centroids
+        plt.figure()
+        plt.scatter(data[:,0], data[:,1], c=labels, cmap='viridis')
+        plt.scatter(centroids[:,0], centroids[:,1], color='red')
+        plt.savefig(f"{folder}/{success}_{i}_2.png")
+
+    # if success == "B":
+    #     print(centroids)
+    
+    plt.show()
+
+
+
+def run():
+    initial_points = get_initial_points()
+    all_points = []
+    for point in initial_points:
+        all_points += get_random_within_r(point, 10)
+    all_points += initial_points
+    # plot_points(all_points)
+    kmean_n_step(all_points)
+
+
+run()
+
